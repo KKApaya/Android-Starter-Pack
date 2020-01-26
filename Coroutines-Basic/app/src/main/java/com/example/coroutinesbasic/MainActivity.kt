bf34e72810c66a245f1313920e98e8365bc25c5e
KKApaya <apaya1606@gmail.com>
2020-01-26 17:19:25 +0530

    First commit

---
+++ Coroutines-Basic/app/src/main/java/com/example/coroutinesbasic/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.coroutinesbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    private val RESULT_1="Result #1"
+    private val RESULT_2="Result #2"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        click_button.setOnClickListener {
+            CoroutineScope(IO).launch {
+                getFakeApi()
+            }
+
+        }
+    }
+    private  suspend fun getFakeApi(){
+
+        //First Job
+        val result1 = getResult1FromApi()
+        setTextMainThread(result1)
+
+        //Second Job
+        val result2=getResult2FromApi()
+        setTextMainThread(result2)
+    }
+    private suspend fun setTextMainThread(input:String){
+        //Switching context from IO to Main
+        withContext(Main){
+            val newText = text.text.toString()+"\n$input"
+            text.text = newText
+        }
+    }
+    private suspend fun getResult1FromApi():String{
+        logThread("getResult1FromApi")
+        delay(1000)
+        return RESULT_1
+    }
+    private suspend fun getResult2FromApi():String{
+        logThread("getResult2FromApi")
+        delay(1000)
+        return RESULT_2
+    }
+
+    private fun logThread(methodName:String){
+        println("debug ${methodName}:${Thread.currentThread().name}")
+    }
+
+}
