8cb278879994e3fe412a5241e8c5d59f00e864e1
kkapaya <kkapaya1606@gmail.com>
2020-03-21 21:27:45 +0530

    Set Style in Notifications

--- Notification/app/src/main/java/com/prasan/notification/MainActivity.kt
+++ Notification/app/src/main/java/com/prasan/notification/MainActivity.kt
@@ -2,6 +2,8 @@ package com.prasan.notification
 
 import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
@@ -19,10 +21,10 @@ class MainActivity : AppCompatActivity() {
         notificationManager = NotificationManagerCompat.from(this)
         var intent = Intent(this,MainActivity::class.java)
         var contentIntent = PendingIntent.getActivity(this,0,intent,0)
-
         var broadcastIntent = Intent(this,NotificationReciver::class.java)
-
         var actionIntent = PendingIntent.getBroadcast(this,0,broadcastIntent,PendingIntent.FLAG_UPDATE_CURRENT)
+        var largeIcon = BitmapFactory.decodeResource(resources,R.drawable.itachi)
+
 
         binding.buttonChannel1.setOnClickListener {
             var titleStr = binding.editTitle.text.toString()
@@ -32,6 +34,11 @@ class MainActivity : AppCompatActivity() {
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(titleStr)
                 .setContentText(msgStr)
+                .setLargeIcon(largeIcon)
+                .setStyle(NotificationCompat.BigTextStyle()
+                    .bigText(getString(R.string.dummy_text))
+                    .setBigContentTitle("Big Content Title")
+                    .setSummaryText("Summary text"))
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .setContentIntent(contentIntent)
                 .setAutoCancel(true)
@@ -41,6 +48,8 @@ class MainActivity : AppCompatActivity() {
 
             notificationManager.notify(1,notification)
         }
+
+
         binding.buttonChannel2.setOnClickListener {
             var titleStr = binding.editTitle.text.toString()
             var msgStr = binding.editMessage.text.toString()
@@ -48,6 +57,17 @@ class MainActivity : AppCompatActivity() {
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(titleStr)
                 .setContentText(msgStr)
+                    // 7 Lines max will be displayed in the collapsed notification
+                .setStyle(NotificationCompat.InboxStyle()
+                    .addLine("Line 1")
+                    .addLine("Line 2")
+                    .addLine("Line 3")
+                    .addLine("Line 4")
+                    .addLine("Line 5")
+                    .addLine("Line 6")
+                    .addLine("Line 7")
+                    .setBigContentTitle("Big Content Title")
+                    .setSummaryText("Summary text"))
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .build()
 
